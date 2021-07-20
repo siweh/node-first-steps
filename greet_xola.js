2f36bd7242a4ad6bea19790b4e3942649de4e160
siweh <sanemadesi@gmail.com>
2021-07-20 11:12:52 +0200

    Combined the function using figlet and chalk

--- greet_xola.js
+++ greet_xola.js
@@ -6,3 +6,15 @@ const greet = require('./greet');
 
 const styledMessage = chalk.bgBlue.black(greet('Xola'));
 console.log(styledMessage)
+
+//Using figlet to style a string in ASCII art
+const figlet = require('figlet')
+
+figlet('Hello, Sanelisiwe', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
